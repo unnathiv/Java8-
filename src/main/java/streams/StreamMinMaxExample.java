657aef8397e6baba13f055feceff5781943b38af
unnathiv <unnathi1894@gmail.com>
2021-04-13 17:04:21 -0500

    Minimum element using Optional

--- src/main/java/streams/StreamMinMaxExample.java
+++ src/main/java/streams/StreamMinMaxExample.java
@@ -4,6 +4,7 @@ import data.StudentDataBase;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamMinMaxExample {
 
@@ -13,11 +14,27 @@ public class StreamMinMaxExample {
 
     }
 
+    private static Optional<Integer> findMinValue(List<Integer> integerList){
+        return integerList.stream()
+                .reduce((x,y) -> x<y ? x :y);
+
+    }
+
+
     public static void main(String[] args) {
 
       List<Integer> integerList = Arrays.asList(6,7,8,9,10);
        int maxValue = findMaxValue(integerList);
         System.out.println("Max value :" +maxValue);
+
+        Optional<Integer> minValueOptional = findMinValue(integerList);
+        System.out.println("Min value Optional :" + minValueOptional);
+
+        if(minValueOptional.isPresent()){
+            System.out.println("The minimun value is :" + minValueOptional.get());
+        }else{
+            System.out.println("Empty input");
+        }
 
     }
 }
